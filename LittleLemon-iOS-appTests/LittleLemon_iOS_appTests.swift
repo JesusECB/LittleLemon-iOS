67d835eb8d6191fd228c8f24ff796d8161a9faa8
JesusECB <jecb.st@gmail.com>
2025-05-11 18:53:44 +0200

    Initial Commit

---
+++ LittleLemon-iOS-appTests/LittleLemon_iOS_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemon_iOS_appTests.swift
+//  LittleLemon-iOS-appTests
+//
+//  Created by Jesús Enrique on 11/5/25.
+//
+
+import Testing
+@testable import LittleLemon_iOS_app
+
+struct LittleLemon_iOS_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
